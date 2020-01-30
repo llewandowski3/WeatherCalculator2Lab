d4b430f20902718755aae50f3b11cc43d34bf0d2
llewandowski3 <59981831+llewandowski3@users.noreply.github.com>
2020-01-30 16:27:08 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -12,26 +12,49 @@ namespace WeatherCalculator2
             int windSpeed;
             double windChill;
             string input = "";
+            int humidity;
+            
 
             // Get the temperature from the user
-            Console.WriteLine("Enter the temperature");
+            Console.WriteLine("Enter the temperature in Fahrenheit");
 
             input = Console.ReadLine();
 
             temperature = int.Parse(input);
+            if (temperature < 50)
+            {
 
-            // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+                // Get the windspeed from the user
+                Console.WriteLine("Enter the wind speed");
 
-            input = Console.ReadLine();
+                input = Console.ReadLine();
 
-            windSpeed = int.Parse(input);
+                windSpeed = int.Parse(input);
 
-            // Calculate the wind chill
+                // Calculate the wind chill
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
-            Console.WriteLine("The wind chill is " + windChill);
+                Console.WriteLine("The wind chill is " + windChill);
+            }
+            else
+            {
+                Console.WriteLine("Enter the Relitive Humidity");
+
+                input = Console.ReadLine();
+
+                humidity = int.Parse(input);
+
+                // Calculate Heat Index
+                double HI = .5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (humidity * 0.094));
+
+                Console.WriteLine("The heat index is"+ HI);
+            }
+
+            
+
+
+
         }
     }
 }
